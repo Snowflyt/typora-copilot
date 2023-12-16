616d0c359d324c58d62eec62cab0dce0f21d3c2b
Snowflyt <gaoge011022@163.com>
2023-12-17 00:59:35 +0800

    🐳 chore(vitest.config.ts): Update path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   resolve: {
     alias: {
+      "@modules": path.resolve(__dirname, "src/modules"),
       "@": path.resolve(__dirname, "src"),
       "@test": path.resolve(__dirname, "test"),
     },
