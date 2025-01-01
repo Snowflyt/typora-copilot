a8b3ae3ed85f6fa2284907ac29d0c931c00582aa
Snowflyt <gaoge011022@163.com>
2025-01-01 14:55:05 +0800

    🦄 refactor(components): Generalize `SettingsPanel`

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable sonarjs/no-duplicate-string */
-
 import { useSignal } from "@preact/signals";
+import { mapValues } from "radash";
+import { kebabCase } from "string-ts";
 
 import ModalBody from "./ModalBody";
 import ModalCloseButton from "./ModalCloseButton";
@@ -10,8 +10,46 @@ import ModalTitle from "./ModalTitle";
 import ModalHeader from "./ModelHeader";
 import Switch from "./Switch";
 
+import type { Settings } from "@/settings";
+import type { _Id } from "@/types/tools";
+import type { Signal } from "@preact/signals";
+
 import { t } from "@/i18n";
 import { settings } from "@/settings";
+import { entriesOf, keysOf } from "@/utils/tools";
+
+type SettingControl<K extends keyof Settings> = (
+  key: K,
+  signal: Signal<Settings[K]>,
+) => preact.JSX.Element;
+type TypedSettingControl<T> = SettingControl<
+  keyof { [K in keyof Settings as Settings[K] extends T ? K : never]: void }
+>;
+
+const BooleanSettingControl: TypedSettingControl<boolean> = (key, signal) => (
+  <Switch
+    value={signal.value}
+    onChange={(value) => {
+      signal.value = value;
+      settings[key] = value;
+    }}
+  />
+);
+
+type Categories = Record<string, { [K in keyof Settings]: SettingControl<K> }>;
+const categories = {
+  general: {
+    disableCompletions: BooleanSettingControl,
+    useInlineCompletionTextInSource: BooleanSettingControl,
+    useInlineCompletionTextInPreviewCodeBlocks: BooleanSettingControl,
+  },
+} as const satisfies Categories;
+
+type CategoriesSignals<C extends Categories> = _Id<{
+  [K in keyof C]: {
+    [P in keyof C[K]]: C[K][P] extends SettingControl<infer K> ? Signal<Settings[K]> : never;
+  };
+}>;
 
 export interface SettingsPanelProps {
   open?: boolean;
@@ -19,10 +57,9 @@ export interface SettingsPanelProps {
 }
 
 const SettingsPanel: FC<SettingsPanelProps> = ({ onClose }) => {
-  const disableCompletion = useSignal(settings.disableCompletions);
-  const useInlineCompletionTextInSource = useSignal(settings.useInlineCompletionTextInSource);
-  const useInlineCompletionTextInPreviewCodeBlocks = useSignal(
-    settings.useInlineCompletionTextInPreviewCodeBlocks,
+  const selectedCategory = useSignal(Object.keys(categories)[0] as keyof typeof categories);
+  const signals: CategoriesSignals<typeof categories> = mapValues(categories, (category) =>
+    mapValues(category, (_, key) => useSignal(settings[key])),
   );
 
   return (
@@ -33,11 +70,21 @@ const SettingsPanel: FC<SettingsPanelProps> = ({ onClose }) => {
           <ModalCloseButton onClick={onClose} />
         </ModalHeader>
         <ModalBody style={{ paddingTop: "1rem", display: "flex", flexDirection: "row" }}>
-          <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-start" }}>
-            <MenuButton selected>
-              <SettingsIcon size={18} />
-              <span style={{ marginLeft: "0.375rem" }}>{t("settings-panel.general.title")}</span>
-            </MenuButton>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
+            {keysOf(categories).map((category, i, arr) => (
+              <MenuButton
+                key={category}
+                selected={selectedCategory.value === category}
+                onClick={() => {
+                  selectedCategory.value = category;
+                }}
+                style={{ marginBottom: i === arr.length - 1 ? "0" : "0.5rem" }}>
+                <SettingsIcon size={18} />
+                <span style={{ marginLeft: "0.375rem" }}>
+                  {t(`settings-panel.${category}.title`)}
+                </span>
+              </MenuButton>
+            ))}
           </div>
 
           <div
@@ -51,110 +98,49 @@ const SettingsPanel: FC<SettingsPanelProps> = ({ onClose }) => {
               display: "flex",
               flexDirection: "column",
             }}>
-            <div style={{ width: "100%" }}>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}>
-                <span>{t("settings-panel.general.disable-completions.label")}</span>
-                <Switch
-                  value={disableCompletion.value}
-                  onChange={(value) => {
-                    disableCompletion.value = value;
-                    settings.disableCompletions = value;
-                  }}
-                />
-              </div>
-              <div
-                style={{
-                  marginTop: "0.5rem",
-                  fontSize: "0.75rem",
-                  lineHeight: 1,
-                  opacity: 0.75,
-                }}>
-                {t("settings-panel.general.disable-completions.description")}
-              </div>
-            </div>
-
-            <hr style={{ width: "100%", margin: "1.375rem 0 1rem 0" }} />
-
-            <div style={{ width: "100%" }}>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}>
-                <span>
-                  {t("settings-panel.general.use-inline-completion-text-in-source.label")}
-                </span>
-                <Switch
-                  value={useInlineCompletionTextInSource.value}
-                  onChange={(value) => {
-                    useInlineCompletionTextInSource.value = value;
-                    settings.useInlineCompletionTextInSource = value;
-                  }}
-                />
-              </div>
-              <div
-                style={{
-                  marginTop: "0.5rem",
-                  fontSize: "0.75rem",
-                  lineHeight: 1,
-                  opacity: 0.75,
-                }}>
-                {t("settings-panel.general.use-inline-completion-text-in-source.description")}
-              </div>
-            </div>
-
-            <hr style={{ width: "100%", margin: "1.375rem 0 1rem 0" }} />
-
-            <div style={{ width: "100%" }}>
-              <div
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}>
-                <span>
-                  {t(
-                    "settings-panel.general.use-inline-completion-text-in-preview-code-blocks.label",
+            {entriesOf(categories[selectedCategory.value]).map(([key, control], i, arr) => (
+              <>
+                <div key={key} style={{ width: "100%" }}>
+                  <div
+                    style={{
+                      width: "100%",
+                      display: "flex",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}>
+                    <span>
+                      {t(`settings-panel.${selectedCategory.value}.${kebabCase(key)}.label`)}
+                    </span>
+                    {control(key, signals[selectedCategory.value][key])}
+                  </div>
+                  <div
+                    style={{
+                      marginTop: "0.5rem",
+                      fontSize: "0.75rem",
+                      lineHeight: 1,
+                      opacity: 0.75,
+                    }}>
+                    {t(`settings-panel.${selectedCategory.value}.${kebabCase(key)}.description`)}
+                  </div>
+                  {t.test(`settings-panel.${selectedCategory.value}.${kebabCase(key)}.warning`) && (
+                    <div
+                      style={{
+                        marginTop: "0.5rem",
+                        fontSize: "0.75rem",
+                        lineHeight: 1,
+                        color: "#ef4444",
+                      }}>
+                      {t.tran(`settings-panel.${selectedCategory.value}.${kebabCase(key)}.warning`)}
+                    </div>
                   )}
-                </span>
-                <Switch
-                  value={useInlineCompletionTextInPreviewCodeBlocks.value}
-                  onChange={(value) => {
-                    useInlineCompletionTextInPreviewCodeBlocks.value = value;
-                    settings.useInlineCompletionTextInPreviewCodeBlocks = value;
-                  }}
-                />
-              </div>
-              <div
-                style={{ marginTop: "0.5rem", fontSize: "0.75rem", lineHeight: 1, opacity: 0.75 }}>
-                {t(
-                  "settings-panel.general.use-inline-completion-text-in-preview-code-blocks.description",
+                </div>
+
+                {i !== arr.length - 1 && (
+                  <hr style={{ width: "100%", margin: "1.375rem 0 1rem 0" }} />
                 )}
-              </div>
-              <div
-                style={{
-                  marginTop: "0.5rem",
-                  fontSize: "0.75rem",
-                  lineHeight: 1,
-                  color: "#ef4444",
-                }}>
-                {t(
-                  "settings-panel.general.use-inline-completion-text-in-preview-code-blocks.warning",
-                )}
-              </div>
-            </div>
+              </>
+            ))}
           </div>
         </ModalBody>
       </ModalContent>
@@ -162,7 +148,11 @@ const SettingsPanel: FC<SettingsPanelProps> = ({ onClose }) => {
   );
 };
 
-const MenuButton: FC<{ selected?: boolean }> = ({ children, selected = false }) => {
+const MenuButton: FC<{
+  selected?: boolean;
+  onClick?: () => void;
+  style?: preact.JSX.CSSProperties;
+}> = ({ children, onClick, selected = false, style: additionalStyle }) => {
   const style: preact.JSX.CSSProperties = {
     width: "100%",
     fontSize: "0.875rem",
@@ -178,10 +168,14 @@ const MenuButton: FC<{ selected?: boolean }> = ({ children, selected = false }) 
     justifyContent: "flex-start",
     cursor: selected ? "default" : "pointer",
     backgroundColor: selected ? "var(--item-hover-bg-color)" : "transparent",
+    ...(selected ? { pointerEvents: "none" } : {}),
+    ...additionalStyle,
   };
   return selected ?
-      <div style={style}>{children}</div>
-    : <button type="button" className="unset-button" style={style}>
+      <button className="unset-button" style={style} disabled>
+        {children}
+      </button>
+    : <button type="button" className="unset-button" style={style} onClick={onClick}>
         {children}
       </button>;
 };
